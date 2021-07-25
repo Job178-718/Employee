d7a4a21b2bfd9775ae3894968e10d02dcc658d3f
Job178-718 <58872930+Job178-718@users.noreply.github.com>
2021-07-25 16:47:17 +0800

    sql

---
+++ sql/admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO `admin` VALUES (1, 'aaa', 'aaa', 1, '23056479@qq.com', '2021-03-07 23:34:19', '君莫笑', '847d73ce099e4332af5ce8a1ef6df743.jpeg');
+INSERT INTO `admin` VALUES (2, 'bbb', 'bbb', 0, '2365878@qq.com', '2021-06-02 09:34:38', '妃子笑', 'favicon.ico');
---
+++ sql/tnl_dept.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tnl_dept` VALUES (1, '销售部');
+INSERT INTO `tnl_dept` VALUES (2, '设计部');
+INSERT INTO `tnl_dept` VALUES (3, '开发部');
+INSERT INTO `tnl_dept` VALUES (4, '测试部');
